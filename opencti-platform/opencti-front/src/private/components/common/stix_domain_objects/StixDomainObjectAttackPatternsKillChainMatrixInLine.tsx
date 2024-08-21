2b125eaac8c8edca83eb82d32e54d7e538b54207
Gwendoline Favre-Felix <gwendoline.favre-felix@filigran.io>
2024-08-21 16:42:54 +0200

    [frontend] Modifying fragments (#4230)

--- opencti-platform/opencti-front/src/private/components/common/stix_domain_objects/StixDomainObjectAttackPatternsKillChainMatrixInLine.tsx
+++ opencti-platform/opencti-front/src/private/components/common/stix_domain_objects/StixDomainObjectAttackPatternsKillChainMatrixInLine.tsx
@@ -2,14 +2,24 @@ import React, { FunctionComponent } from 'react';
 import {
   StixDomainObjectAttackPatternsKillChainContainer_data$data,
 } from '@components/common/stix_domain_objects/__generated__/StixDomainObjectAttackPatternsKillChainContainer_data.graphql';
-import AttackPatternsMatrixLines from '@components/techniques/attack_patterns/AttackPatternsMatrixLines';
+import AttackPatternsMatrixLines, { attackPatternsMatrixLinesQuery } from '@components/techniques/attack_patterns/AttackPatternsMatrixLines';
 import { NarrativesLinesPaginationQuery$variables } from '@components/techniques/narratives/__generated__/NarrativesLinesPaginationQuery.graphql';
 import ToolBar from '@components/data/ToolBar';
-import { AttackPatternNode } from '@components/techniques/attack_patterns/AttackPatternsMatrixLine';
+import { AttackPatternsMatrixLineDummy } from '@components/techniques/attack_patterns/AttackPatternsMatrixLine';
+import {
+  AttackPatternsMatrixLinesPaginationQuery,
+  AttackPatternsMatrixLinesPaginationQuery$variables,
+} from '@components/techniques/attack_patterns/__generated__/AttackPatternsMatrixLinesPaginationQuery.graphql';
+import AttackPatternCreation from '@components/techniques/attack_patterns/AttackPatternCreation';
+import { AttackPatternsMatrixLine_node$data } from '@components/techniques/attack_patterns/__generated__/AttackPatternsMatrixLine_node.graphql';
 import useEntityToggle from '../../../../utils/hooks/useEntityToggle';
 import ListLines from '../../../../components/list_lines/ListLines';
 import { usePaginationLocalStorage } from '../../../../utils/hooks/useLocalStorage';
 import { emptyFilterGroup, useBuildEntityTypeBasedFilterContext } from '../../../../utils/filters/filtersUtils';
+import useQueryLoading from '../../../../utils/hooks/useQueryLoading';
+import Security from '../../../../utils/Security';
+import { KNOWLEDGE_KNPARTICIPATE, KNOWLEDGE_KNUPDATE } from '../../../../utils/hooks/useGranted';
+import useHelper from '../../../../utils/hooks/useHelper';
 
 const LOCAL_STORAGE_KEY = 'StixDomainObjectAttackPatternsKillChainMatrixInline';
 
@@ -22,7 +32,9 @@ const StixDomainObjectAttackPatternsKillChainMatrixInline: FunctionComponent<Sti
     data,
   },
 ) => {
-  const { viewStorage, helpers } = usePaginationLocalStorage<NarrativesLinesPaginationQuery$variables>(
+  const { isFeatureEnable } = useHelper();
+  const isFABReplaced = isFeatureEnable('FAB_REPLACEMENT');
+  const { viewStorage, helpers, paginationOptions } = usePaginationLocalStorage<NarrativesLinesPaginationQuery$variables>(
     LOCAL_STORAGE_KEY,
     {
       searchTerm: '',
@@ -51,10 +63,18 @@ const StixDomainObjectAttackPatternsKillChainMatrixInline: FunctionComponent<Sti
     deSelectedElements,
     selectAll,
     handleToggleSelectAll,
-  } = useEntityToggle<AttackPatternNode>(LOCAL_STORAGE_KEY);
+  } = useEntityToggle<AttackPatternsMatrixLine_node$data>(LOCAL_STORAGE_KEY);
 
   const contextFilters = useBuildEntityTypeBasedFilterContext('Attack-Pattern', filters);
+  const queryPaginationOptions = {
+    ...paginationOptions,
+    filters: contextFilters,
+  } as unknown as AttackPatternsMatrixLinesPaginationQuery$variables;
 
+  const queryRef = useQueryLoading<AttackPatternsMatrixLinesPaginationQuery>(
+    attackPatternsMatrixLinesQuery,
+    queryPaginationOptions,
+  );
   const dataColumns = {
     killChainPhase: {
       label: 'Kill chain phase',
@@ -87,7 +107,8 @@ const StixDomainObjectAttackPatternsKillChainMatrixInline: FunctionComponent<Sti
       isSortable: true,
     },
   };
-
+  console.log('data in StixDomainObjectAttackPatternsKillChainMatrixInline', data);
+  console.log('attackPatterns in StixDomainObjectAttackPatternsKillChainMatrixInline', attackPatterns);
   return (
     <>
       <ListLines
@@ -97,20 +118,40 @@ const StixDomainObjectAttackPatternsKillChainMatrixInline: FunctionComponent<Sti
         dataColumns={dataColumns}
         handleToggleSelectAll={handleToggleSelectAll}
         selectAll={selectAll}
+        paginationOptions={queryPaginationOptions}
         iconExtension={true}
+        createButton={isFABReplaced && <Security needs={[KNOWLEDGE_KNUPDATE, KNOWLEDGE_KNPARTICIPATE]}>
+          <AttackPatternCreation paginationOptions={queryPaginationOptions} />
+        </Security>}
       >
-        <AttackPatternsMatrixLines
-          attackPatterns={attackPatterns}
-          dataColumns={dataColumns}
-          onLabelClick={helpers.handleAddFilter}
-          numberOfSelectedElements={numberOfSelectedElements}
-          handleClearSelectedElements={handleClearSelectedElements}
-          selectedElements={selectedElements}
-          deSelectedElements={deSelectedElements}
-          onToggleEntity={onToggleEntity}
-          selectAll={selectAll}
-          setNumberOfElements={helpers.handleSetNumberOfElements}
-        />
+        {queryRef && (
+        <React.Suspense
+          fallback={
+            <>
+              {Array(20)
+                .fill(0)
+                .map((_, idx) => (
+                  <AttackPatternsMatrixLineDummy key={idx} dataColumns={dataColumns} />
+                ))}
+            </>
+                }
+        >
+          <AttackPatternsMatrixLines
+            attackPatterns={attackPatterns}
+            queryRef={queryRef}
+            paginationOptions={queryPaginationOptions}
+            dataColumns={dataColumns}
+            onLabelClick={helpers.handleAddFilter}
+            numberOfSelectedElements={numberOfSelectedElements}
+            handleClearSelectedElements={handleClearSelectedElements}
+            selectedElements={selectedElements}
+            deSelectedElements={deSelectedElements}
+            onToggleEntity={onToggleEntity}
+            selectAll={selectAll}
+            setNumberOfElements={helpers.handleSetNumberOfElements}
+          />
+        </React.Suspense>
+        )}
       </ListLines>
       <ToolBar
         selectedElements={selectedElements}
